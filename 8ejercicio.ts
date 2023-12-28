d931ed943080bca75f1b8fee4617a6fe9f609acb
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 14:33:44 -0500

    8ejercicio.ts

---
+++ 8ejercicio.ts
@@ -0,0 +1,25 @@
+let carro = {
+  puertas: 4,
+  marca: 'chevrolet',
+  color: 'rojo vino',
+  anio: 2005,
+}
+
+let Tv = {
+  pulgadas: 75,
+  marca: 'LG',
+  anio: 2023,
+  tipo: 'Led'
+}
+
+let videosYoutube = {
+  videos: 8,
+  creador: 'Bad Bunny',
+  comentarios: ['Buena cancion','Like aqui para...']
+  likes: '1M',
+  dislikes: 300,
+}
+
+console.log (carro);
+console.log (Tv);
+console.log (videosYoutube);

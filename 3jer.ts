e02aaa98514753c33e25ccbe9916fcc8f9fe8876
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 15:18:15 -0500

    Create 3jer.ts

---
+++ 3jer.ts
@@ -0,0 +1,15 @@
+console.log('Inicio de programa');
+
+export let isTired:boolean = false;
+
+let grade:number = 78;
+
+if ( grade >= 60 ) {
+    console.log('El alumno aprueba la clase!');
+} else if( grade > 50 ) {
+    console.log('Por favor estudie más!')
+} else {
+    console.log('El alumno NO aprueba la clase');
+}
+
+console.log('Fin de programa');

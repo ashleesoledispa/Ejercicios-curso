3b21b04c1857135afc25bb2d77e0f89c6ecc49d0
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 15:15:06 -0500

    Create 6ejercicio.ts

---
+++ 6ejercicio.ts
@@ -0,0 +1,6 @@
+let personas: string[] = ['Ashlee','Soledispa','Gabriel'];
+let flores: string[] = ['Rosas','Girasoles','Lirios','Flor azul'];
+
+for ( let i = 0; i < flores.length; i++ ){
+  console.log( flores[i] );
+}

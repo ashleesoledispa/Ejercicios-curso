cfed00b7c917c34576e877defb376bb28362fc4c
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 15:15:49 -0500

    Create cuatrotarea.ts

---
+++ cuatrotarea.ts
@@ -0,0 +1,9 @@
+let personas: string[] = ['Ashlee','Soledispa','Gabriel'];
+let salario: number[] = [450, 1200, 1000];
+
+for ( let i = 0; i < personas.length; i++ ){
+  let personas = personas[i];
+  let salario = salario[i];
+  
+  console.log( personas + ' tiene un salario de '+ salario);
+}

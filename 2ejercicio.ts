d3eab2c7fc11cd6c007ac67a79d973331770c3e4
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 14:36:34 -0500

    Create 2ejercicio.ts

---
+++ 2ejercicio.ts
@@ -0,0 +1,18 @@
+export let nombre: string = 'Ashlee';
+let apellido: string  = ' Soledispa';
+
+let nombreCompleto: string = nombre + apellido;
+console.log(nombreCompleto);
+
+let number1: number = 8.5;
+let number2: number = 10.3;
+
+console.log( number1 + number2 );
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+console.log('isActive', isActive );
+console.log('isRunning', isRunning );
+console.log('hasKids', hasKids

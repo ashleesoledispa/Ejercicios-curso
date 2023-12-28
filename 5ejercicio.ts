ec97185c023a92e27465e8496636a447f8ce67a8
ashleesoledispa <138799009+ashleesoledispa@users.noreply.github.com>
2023-12-28 14:35:26 -0500

    Create 5ejercicio.ts

---
+++ 5ejercicio.ts
@@ -0,0 +1,22 @@
+Ciclo While
+let tanque = 23;
+
+while ( tanque > 0 ) {
+    console.log('Gasolina restante', tanque );
+
+    tanque--;
+}
+
+console.log('Ya no tiene gasolina');
+
+// Ciclo Do While
+// export let tanque = 23;
+
+// do {
+//     console.log('Gasolina restante', tanque );
+
+//     tanque--;
+
+// } while ( tanque > 0 );
+
+// console.log('Ya no tiene gasolina');
